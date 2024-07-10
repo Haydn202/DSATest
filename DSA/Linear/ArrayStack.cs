d402f6c984ca29e443112f68a72cb371c44705f1
Haydn Thomson <haydn.thomson@ospri.co.nz>
2024-07-11 09:06:19 +1200

    add array based stack

---
+++ DSA/Linear/ArrayStack.cs
@@ -0,0 +1,51 @@
+﻿namespace DSA.Linear;
+
+public class ArrayStack<T>
+{
+    private Array _stack = new T[10];
+    private int _size = 0;
+
+    public void Push(T value)
+    {
+        if (_size == _stack.Length)
+        {
+            IncreaseStackLength();
+        }
+        
+        _stack.SetValue(value, _size);
+        ++_size;
+    }
+
+    public object? Pop()
+    {
+        if (_size == 0)
+        {
+            throw new InvalidOperationException("Empty");
+        }
+
+        --_size;
+        return _stack.GetValue(_size);
+    }
+
+    public object? Peek()
+    {
+        if (_size == 0)
+        {
+            throw new InvalidOperationException("Empty");
+        }
+        
+        return _stack.GetValue(_size - 1);
+    }
+
+    public void Clear()
+    {
+        _size = 0;
+    }
+
+    private void IncreaseStackLength()
+    {
+        var resizedStack = new T[_size * 2];
+        _stack.CopyTo(resizedStack, 0);
+        _stack = resizedStack;
+    }
+}
--- DSATest/Program.cs
+++ DSATest/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-var linkedList = new DSA.Linear.LinkedList<int>();
+/*var linkedList = new DSA.Linear.LinkedList<int>();
 
 linkedList.Append(1);
 linkedList.Append(3);
@@ -9,4 +9,14 @@ linkedList.Insert(2, 1);
 
 Console.WriteLine(linkedList.Pop());
 
-linkedList.PrintList();
+linkedList.PrintList();*/
+
+var stack = new DSA.Linear.ArrayStack<int>();
+
+stack.Push(1);
+stack.Push(2);
+stack.Push(3);
+Console.WriteLine(stack.Peek());
+Console.WriteLine(stack.Pop());
+Console.WriteLine(stack.Pop());
+Console.WriteLine(stack.Pop());
